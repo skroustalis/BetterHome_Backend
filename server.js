6d9a037728ad0b10c46438bc6ade3cb865fbf0da
sokratis <sokroustalis@gmail.com>
2022-11-30 15:59:22 +0200

    from new branch

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter =require('./routes/houses')
+const housesRouter =require('./routes/houseRoutes')
+app.use('/houses', housesRouter)
 
 app.set('view engine','ejs')
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.json())
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -22,6 +24,6 @@ mongoose.connect(process.env.DATABASE_URL, {
     db.on('error', error => console.error(error))
     db.once('open', () => console.log('Connected to Mongoose'))
 
-app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+
+app.listen(3000, () => console.log('server Started'))
