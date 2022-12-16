efe6dd3c066b45773acc5c744c8f495ccd714f30
sokratis <sokroustalis@gmail.com>
2022-12-16 21:13:16 +0200

    update server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
 
 const express = require('express')
 const bodyparser=require('body-parser')
@@ -5,7 +8,7 @@ const dotenv = require('dotenv')
 const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
-dotenv.config({path:'.env'})
+// dotenv.config({path:'.env'})
 
 const db = require('./database/connection')
 
@@ -25,4 +28,6 @@ app.use(bodyparser.urlencoded({
 const housesRouter =require('./routes/houseRoutes')
 app.use('/', housesRouter)
 
-app.listen(process.env.PORT || 3000, () => console.log('server Started'))
+let port = process.env.PORT || 3000
+
+app.listen(port, () => console.log('server Started'))
